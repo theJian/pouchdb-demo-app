df6ccbd03f13d9a6f8139f89b02cad5d8c4d166e
theJian <thejianmail@gmail.com>
2017-04-23 12:18:39 +0800

    add store

---
+++ public/js/store.js
@@ -0,0 +1,47 @@
+class Store {
+  constructor(name) {
+    this.db = new PouchDB(name);
+  }
+
+  getAll() {
+    // get all items from storage
+    // see: https://pouchdb.com/api.html#batch_fetch
+    return this.db.allDocs({
+      include_docs: true
+    })
+    .then(db => {
+      return db.rows.map(row => row.doc)
+    });
+  }
+
+  get(id) {
+    return this.db.get(id);
+  }
+
+  save(item) {
+    return item._id ?
+      this.update(item):
+      this.add(item);
+  }
+
+  add(item) {
+    return this.db.post(item);
+  }
+
+  update(item) {
+    return this.db.get(item._id)
+      .then(updatingItem => {
+        Object.assign(updatingItem, item);
+        return this.db.put(updatingItem);
+      })
+  }
+
+  remove(id) {
+    return this.db.get(id)
+      .then(item => {
+        return this.db.remove(item);
+      })
+  }
+}
+
+window.Store = store;
